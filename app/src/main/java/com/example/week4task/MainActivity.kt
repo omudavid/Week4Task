84bb66888d8ada0d9a9ea2da5b9cd232947fe223
omudavid <omudavid@gmail.com>
2020-08-12 14:22:11 +0100

    First Commit

---
+++ app/src/main/java/com/example/week4task/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.week4task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toggle.setOnCheckedChangeListener(){_, isChecked ->
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+        }
+    }
+}
